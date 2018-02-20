12ae7bfbe8e784279ad0f11783ff7b50edab15f2
Andre Brandemburg dos Anjos <andrebrandg@gmail.com>
2018-02-19 21:10:37 -0300

    Inital commit

---
+++ src/AddonUpdater/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CommandLine;
+
+namespace AddonUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<ProgramOptions>(args)
+                .WithParsed(opts => {
+                    
+                })
+                .WithNotParsed( errors => { System.Console.WriteLine(" ERROR "); });
+        }
+    }
+}
---
+++ src/AddonUpdater/ProgramOptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using CommandLine;
+
+namespace AddonUpdater
+{
+    class ProgramOptions
+    {
+        [Option('f', "file", Required = true, HelpText = "Input files to be processed")]
+        public string ConfigFile { get; set; }       
+    }
+}
